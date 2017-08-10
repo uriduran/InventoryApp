2bf96be93c6c33b0a5ace37518141ed1e27d7f27
Uriel Duran <uduran@hawk.iit.edu>
2017-08-10 09:55:31 -0500

    database inventory submission

---
+++ database/post.php
@@ -0,0 +1,17 @@
+<?php
+
+require 'Connection.php';
+
+var_dump($_REQUEST);
+
+var_dump((int)$_REQUEST['Category']);
+
+$category = $_REQUEST[''];
+$gNumber = $_REQUEST[''];
+$pNumber = $_REQUEST[''];
+$vendor = $_REQUEST[''];
+$price = $_REQUEST[''];
+$description = $_REQUEST[''];
+$location = $_REQUEST[''];
+$quantity = $_REQUEST[''];
+$capacity = $_REQUEST[''];
