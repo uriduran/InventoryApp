e36b734386000f12f1c8bc17daa5b26cb63f1cb2
Uriel Duran <uduran@hawk.iit.edu>
2017-08-10 10:51:22 -0500

    Placed the form details into appropriate datatypes and variables

--- database/post.php
+++ database/post.php
@@ -4,14 +4,25 @@ require 'Connection.php';
 
 var_dump($_REQUEST);
 
-var_dump((int)$_REQUEST['Category']);
 
-$category = $_REQUEST[''];
-$gNumber = $_REQUEST[''];
-$pNumber = $_REQUEST[''];
-$vendor = $_REQUEST[''];
-$price = $_REQUEST[''];
-$description = $_REQUEST[''];
-$location = $_REQUEST[''];
-$quantity = $_REQUEST[''];
-$capacity = $_REQUEST[''];
+
+$category = (int)$_REQUEST['Category'];
+$gNumber = (int)$_REQUEST['GNumber'];
+$pNumber = $_REQUEST['PNumber'];
+$vendor = $_REQUEST['Vendor'];
+$price = (float)$_REQUEST['Price'];
+$description = $_REQUEST['Description'];
+$location = $_REQUEST['Location'];
+$inStock = (int)$_REQUEST['InStock'];
+$capacity = (int)$_REQUEST['Capacity'];
+
+
+$sql = "INSERT INTO InvRegistry (Category,GNumber,PNumber)
+          VALUES ('$category', '$gNumber', '$pNumber')";
+$conn->query($sql);
+
+
+//for test
+print($category."-".$gNumber);
+
+header("Location: ../index.php");
